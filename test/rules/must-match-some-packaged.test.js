0763bbaf1e3820f2487cf89804706ef3daffb816
Scott McGinness <mcginness.s@gmail.com>
2024-04-21 22:58:04 +0100

    Add missing tests for when group is empty and there are defined groups

--- test/rules/must-match-some-packaged.test.js
+++ test/rules/must-match-some-packaged.test.js
@@ -145,6 +145,34 @@ ruleTester.run('must-match', rule, {
       endColumn: 14
     }]
   }, {
+    name: 'Fails if there is an empty group',
+    code: dedent`
+       /**
+        * @group
+        */
+       describe("");`,
+    errors: [{
+      message: 'Group name cannot be empty.',
+      line: 2,
+      column: 4,
+      endLine: 2,
+      endColumn: 10
+    }]
+  }, {
+    name: 'Fails if there is a whitespace-only group',
+    code: dedent`
+       /**
+        * @group    
+        */
+       describe("");`,
+    errors: [{
+      message: 'Group name cannot be empty.',
+      line: 2,
+      column: 4,
+      endLine: 2,
+      endColumn: 14
+    }]
+  }, {
     name: 'Fails with a specific message on the TODO group',
     code: dedent`
         /**
